8b38d387c97589f6f5b2ef0805e9725b0226722c
crm developer <crmdeveloper98@gmail.com>
2020-11-25 07:23:21 +0000

    Project commit

---
+++ BookStore-API/Services/LoggerService.cs
@@ -0,0 +1,18 @@
+﻿using BookStore_API.Contracts;
+using NLog;
+
+namespace BookStore_API.Services
+{
+    public class LoggerService : ILoggerService
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        public void LogInfo(string message) => Logger.Info(message);
+
+        public void LogWarn(string message) => Logger.Warn(message);
+
+        public void LogDebug(string message) => Logger.Debug(message);
+
+        public void LogError(string message) => Logger.Error(message);
+    }
+}
