78fb8a5412c2476eb1af394b502a96ff661faa72
crm developer <crmdeveloper98@gmail.com>
2020-11-29 05:28:12 +0000

    Latest changes-29-11-2020

---
+++ BookStore-API/Controllers/AuthorsController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BookStore_API.Contracts;
+using BookStore_API.DTOs;
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// Endpoint used to interact with the Authors in the book store's database
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IAuthorRepository _authorRepository;
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(IAuthorRepository authorRepository, ILoggerService logger, IMapper mapper)
+        {
+            _authorRepository = authorRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Get All Authors
+        /// </summary>
+        /// <returns>List Of Authors</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAuthors()
+        {
+            try
+            {
+                _logger.LogInfo("Attemptend Get All Authors");
+                var authors = await _authorRepository.FindAll();
+                var response = _mapper.Map<IList<AuthorDTO>>(authors);
+                _logger.LogInfo("Successfully got all Authors");
+                return Ok(response);
+            }
+            catch (Exception e )
+            {
+                return InternalError($"{e.Message} - {e.InnerException}");
+            }
+
+        }
+
+        /// <summary>
+        /// Get an author by id
+        /// </summary>
+        /// <param name="id">author id</param>
+        /// <returns>A single author</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAuthor(int id)
+        {
+            try
+            {
+                _logger.LogInfo($"Attemptend Get Author with id:{id}");
+                var author = await _authorRepository.FindById(id);
+                if (author == null)
+                {
+                    _logger.LogWarn($"Author with id:{id} was not found");
+                    return NotFound();
+                }
+                var response = _mapper.Map<AuthorDTO>(author);
+                _logger.LogInfo($"Successfully got Author with id:{id}");
+                return Ok(response);
+            }
+            catch (Exception e )
+            {
+                return InternalError($"{e.Message} - {e.InnerException}");
+
+            }
+
+        }
+
+        private ObjectResult InternalError(string message)
+        {
+            _logger.LogError(message);
+            return StatusCode(500, "Something went wrong. Please contact the Administrator");
+        }
+
+    }
+}
