20437dbc3d2ea3f57ebf47ef537d4492becaa120
Demo <crmdeveloper98@gmail.com>
2020-12-13 09:55:54 +0000

    Add Bookstore-UI Project

---
+++ BookStore-UI/Contracts/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore_UI.Contracts
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        public Task<T> Get(string url, int id);
+        public Task<IList<T>> Get(string url);
+        public Task<bool> Create(string url, T obj);
+        public Task<bool> Update(string url, T obj);
+        public Task<bool> Delete(string url, int id);
+
+    }
+}
